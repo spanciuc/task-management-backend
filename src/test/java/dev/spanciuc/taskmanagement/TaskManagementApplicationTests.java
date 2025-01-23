14f2b68cf45ac43bc5439f571631313b96f1674a
spanciuc <semion.panciuc@gmail.com>
2025-01-23 09:58:35 +0100

    chore: project init

---
+++ src/test/java/dev/spanciuc/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.spanciuc.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // just checks if context loads
+    }
+
+}
